11c7030b9c682887583d40abbfef20f83cbf7425
Michael Lee <mikecovlee@163.com>
2017-12-24 13:46:17 +0800

    更新构建脚本

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
-mkdir build
+mkdir -p build
 cd build
-mkdir imports
+mkdir -p imports
 gcc -I ../include -fPIC -s -O3 -c ../sqlite3.c
 g++ -std=c++11 -I ../include -shared -fPIC -s -O3 ../sqlite.cpp ./sqlite3.o -o ./imports/sqlite.cse
 rm sqlite3.o
