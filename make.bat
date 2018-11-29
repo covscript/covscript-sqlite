b534965526ba680cf021e78e089bfcb4f6f59a6f
Michael Lee <mikecovlee@163.com>
2018-11-29 23:12:39 +0800

    适配新版本

--- make.bat
+++ make.bat
@@ -2,5 +2,5 @@
 @cd build
 @mkdir imports
 @gcc -I ..\include -fPIC -s -O3 -c ..\sqlite3.c
-@g++ -std=c++11 -I ..\include -shared -static -fPIC -s -O3 ..\sqlite.cpp .\sqlite3.o -o .\imports\sqlite.cse
+@g++ -std=c++11 -I%CS_DEV_PATH%\include -I..\include -shared -static -fPIC -s -O3 ..\sqlite.cpp .\sqlite3.o -o .\imports\sqlite.cse -L%CS_DEV_PATH%\lib -lcovscript
 @del sqlite3.o
