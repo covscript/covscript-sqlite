dcaee63dc454e7e11f2b5bb4e323111fc03a12cc
imkiva <libkernelpanic@gmail.com>
2019-06-18 23:56:48 +0800

    cmake: update cmake for auto build

--- make.bat
+++
@@ -1,6 +0,0 @@
-@mkdir build
-@cd build
-@mkdir imports
-@gcc -I ..\include -fPIC -s -O3 -c ..\sqlite3.c
-@g++ -std=c++14 -I%CS_DEV_PATH%\include -I..\include -shared -static -fPIC -s -O3 ..\sqlite.cpp .\sqlite3.o -o .\imports\sqlite.cse -L%CS_DEV_PATH%\lib -lcovscript
-@del sqlite3.o
